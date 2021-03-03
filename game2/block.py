6bf6d046e88987425684f38350a5ab35abda8ed9
citrus999n <74902876+citrus999n@users.noreply.github.com>
2021-03-03 20:02:49 +0500

    Add files via upload

---
+++ game2/block.py
@@ -0,0 +1,76 @@
+import pygame
+
+RED = (0, 0, 0)
+BLUE = (0, 0, 0)
+
+
+class Block(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height, c_):
+
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        if c_ == 1:
+            self.image.fill(RED)
+            self.image.set_colorkey(RED)
+            c_ = RED
+        else:
+            self.image.fill(BLUE)
+            self.image.set_colorkey(BLUE)
+            c_ = BLUE
+
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        self.rect = self.image.get_rect()
+        self.y2 = self.rect.y
+        self.timer = 0
+
+    def moveUp(self, pixels):
+        self.rect.y -= pixels
+
+        if self.rect.y < 0:
+            self.rect.y = 0
+
+    def moveDown(self, pixels):
+        self.rect.y += pixels
+
+        if self.rect.y > 400:
+            self.rect.y = 400
+
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+
+        if self.rect.x < 0:
+            self.rect.x = 0
+
+        if self.rect.x > 780:
+            self.rect.x = 780
+
+        if 420 < self.rect.x < 430:
+            self.rect.x = 435
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+
+        if self.rect.x < 0:
+            self.rect.x = 0
+
+        if self.rect.x > 780:
+            self.rect.x = 780
+
+        if 370 < self.rect.x < 380:
+            self.rect.x = 365
+
+    def y(self):
+        if self.timer == 0:
+            self.y2 = self.rect.y
+            self.timer += 1
+
+        elif self.timer == 3:
+            self.timer = 0
+
+        else:
+            self.timer += 1
+
+        return self.y2
